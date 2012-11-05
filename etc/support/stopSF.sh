779b980b02af9aac603d316e017731e3747922e0
Jakub Cechacek <jcechace@redhat.com>
2012-11-05 12:15:23 +0100

    support scripts now take $JAVA_HOME as parameter

--- etc/support/stopSF.sh
+++ etc/support/stopSF.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -x $1 "export JAVA_HOME=\$JAVA_HOME; export PATH=\$JAVA_HOME/bin:\$JAVA_HOME/jre/bin:\$PATH; export SFHOME=$2; export SFUSERHOME=$3; $2/bin/sfStopDaemon $1"
+ssh -x $1 "export JAVA_HOME=$4; export PATH=$4/bin:$4/jre/bin:\$PATH; export SFHOME=$2; export SFUSERHOME=$3; $2/bin/sfStopDaemon $1"
