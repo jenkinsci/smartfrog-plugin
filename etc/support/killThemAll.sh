28a9087684b6d40d47d19999c9a9acf9ccbbde97
Richard Achmatowicz <rachmato@redhat.com>
2017-03-07 15:43:23 -0500

    Allow use of plugin with a Jenkins instance running under an arbitrary user name (and certainly not hudson).

--- etc/support/killThemAll.sh
+++ etc/support/killThemAll.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # kill them all and let God sort them out
-ssh -x $1 "ps -u hudson -o pid,args | grep -v -e .*slave.jar$ | grep -o -E ^[0-9,' '][0-9]+ | xargs kill -9"
+ssh -x $1 "ps -u ${USER:-hudson} -o pid,args | grep -v -e .*slave.jar$ | grep -o -E ^[0-9,' '][0-9]+ | xargs kill -9"
